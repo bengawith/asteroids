8e6edea61f888daa0f1951b107415d8bfbf85da5
Ben Gawith <biggawith@gmail.com>
2025-01-01 17:46:47 +0000

    Create scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,60 @@
+from pygame.sprite import Group
+
+from ship import Ship
+
+class Scoreboard:
+
+    def __init__(self, game):
+        # Init the score attributes
+        self.game = game
+        self.screen = game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = game.settings
+        self.stats = game.stats
+        # Font settings for scoring
+        self.text_colour = (215, 215, 215)
+        self.font = self.settings.pixel_font
+
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_ships()
+
+    def prep_score(self):
+        rounded_score = round(self.stats.score, -1)
+        score_str = f"{rounded_score:,}"
+        self.score_image = self.font.render(score_str, True, self.text_colour, self.settings.bg_colour)
+
+        # Display score in top right
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True, self.text_colour, self.settings.bg_colour)
+
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+      
+
+    def prep_ships(self):
+        # Show how many ships are left
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
+    def show_score(self):
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
+
+    def check_high_score(self):
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
