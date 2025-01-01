e07ae6f76410926e36ca537c92b1f210988938bf
Ben Gawith <biggawith@gmail.com>
2025-01-01 18:40:54 +0000

    Create ship.py

---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+from asteroid import Asteroid
+from settings import Settings
+
+
+class Ship(Asteroid):
+    def __init__(self, game, x, y):
+        super().__init__(x, y, Settings.player_radius)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
